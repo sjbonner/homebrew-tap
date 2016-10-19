b8b56fd6a9b5246233e4bffbcb573f063bf84148
Simon Bonner <sbonner6@uwo.ca>
2016-10-18 22:54:05 -0400

    Initial commit

---
+++ mark-on-mac.rb
@@ -0,0 +1,14 @@
+class MarkOnMac < Formula  desc ""
+  homepage "http://warnercnr.colostate.edu/~gwhite/mark/mark.htm"
+  url "https://github.com/sjbonner/mark-on-mac/archive/0.0.1.tar.gz"
+  version "0.0.1"
+  sha256 "ed3ac8a4e92b25ea57092c5769bdcc2b25698668e0e25b8afeef21f802390eee"
+
+
+  def install
+    #system "make", "install" # if this fails, try separate make/make install steps
+    bin.install "mark"
+    bin.install "mark.64.OSX"
+  end
+
+end
