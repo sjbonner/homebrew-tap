cc3a96850d8a6847aad788cbc9710b4aef2fa344
Simon Bonner <sbonner6@uwo.ca>
2018-01-18 23:16:19 -0500

    Really, this time...

--- mark-on-mac.rb
+++ mark-on-mac.rb
@@ -1,13 +1,10 @@
-class MarkOnMac < Formula
+ class MarkOnMac < Formula
   desc "Command-line version of Dr. Gary White's mark-recapture software"
   homepage "http://warnercnr.colostate.edu/~gwhite/mark/mark.htm"
   url "https://github.com/sjbonner/mark-on-mac/archive/0.0.2.tar.gz"
-  version "0.0.2"
-  sha256 "ed3ac8a4e92b25ea57092c5769bdcc2b25698668e0e25b8afeef21f802390eee"
-
+  sha256 "a27eb54acdd0bb2391fdbb8769bdebd28d7422ce019eab569d34840ce1d58f14"
 
   def install
-    #system "make", "install" # if this fails, try separate make/make install steps
     bin.install "mark"
     bin.install "mark.64.OSX"
   end
