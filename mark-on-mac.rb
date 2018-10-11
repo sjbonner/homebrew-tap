8dbed639f3b6eef6c7d0ee4b3d85ede33dd2a556
Simon Bonner <sbonner6@uwo.ca>
2018-10-10 20:31:06 -0400

    Fixed shasum.

--- mark-on-mac.rb
+++ mark-on-mac.rb
@@ -2,7 +2,7 @@ class MarkOnMac < Formula
   desc "Command-line version of Dr. Gary White's mark-recapture software"
   homepage "http://warnercnr.colostate.edu/~gwhite/mark/mark.htm"
   url "https://github.com/sjbonner/mark-on-mac/archive/v0.0.4.tar.gz"
-  sha256 "d279458cd8dee79e0251186e61fae4cc71a98a6d"
+  sha256 "d0f74a442390cb010935b8a0db147341ab8f28b6b9f0d3520c80898320808487"
 
   def install
     bin.install "mark"
