43f5e2e60323407fa259255e689a33e0cefb9a56
Simon Bonner <sbonner6@uwo.ca>
2023-05-07 09:02:54 -0400

    Updated to version 10.1.

--- mark-on-mac.rb
+++ mark-on-mac.rb
@@ -1,9 +1,9 @@
 class MarkOnMac < Formula
   desc "Command-line version of Dr. Gary White's mark-recapture software"
   homepage "http://warnercnr.colostate.edu/~gwhite/mark/mark.htm"
-  url "https://github.com/sjbonner/mark-on-mac/archive/v0.0.8.tar.gz"
-  sha256 "a8937f5fcb882c1056cfe66492ac230659535bd064204acb63d06872e5443fdb"
-
+  url "https://github.com/sjbonner/mark-on-mac/archive/refs/tags/v10.1.1.tar.gz"
+  sha256 "599d079d0aa96fa10ab43b2a97a68f05dd7404e674d3ca63d60f94ed76bdd970"
+ 
   def install
     bin.install "mark"
     bin.install "mark.64.OSX"
